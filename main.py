d2a5958a3074818da6520dca02a4aafc080ee24c
Aituarz <36506361+Aituarz@users.noreply.github.com>
2018-02-20 14:15:08 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import config
 import telebot
 
 bot = telebot.TeleBot(config.token)
-token = '110831855:AAE_GbIeVAUwk11O12vq4UeMnl20iADUtM'
+token = '533314669:AAF_L5ViVpyyEsdy0DKK1SshUwHUQ3bQPi8'
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
